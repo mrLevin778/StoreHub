1b127c359afb0f62466faea1d1a995c5a108b491
mrLevin778 <vitaliy.respect@gmail.com>
2025-02-17 01:23:35 +0200

    Added users.json for saving username/password, in future this data must saved in db Added logic for loading, creating and saving users

--- admin/user.py
+++ admin/user.py
@@ -1,12 +1,42 @@
+import os.path
+
+import bcrypt
+import json
+
 
 class User:
     """Class for creating user with some role"""
+    FILE_PATH = "users.json"
 
-    def __init__(self):
-        pass
+    def __init__(self, username, password, role="user"):
+        self.role = role
+        self.username = username
+        self.password = self._hash_password(password)
+
+    @staticmethod
+    def _hash_password(password):
+        return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
+    @staticmethod
+    def load_users():
+        if not os.path.exists(User.FILE_PATH):
+            with open(User.FILE_PATH, "w") as file:
+                json.dump({}, file)  # create empty json file
+
+        with open(User.FILE_PATH, "r") as file:
+            return json.load(file)
 
     def create_user(self):
-        pass
+        users = self.load_users()
+        if self.username in users:
+            raise ValueError('User is existing!')
+        users[self.username] = {'password': self.password, 'role': self.role}
+        self.save_users(users)
+        print(f'User {self.username} is created!')
+
+    def save_users(users):
+        with open(User.FILE_PATH, "w") as file:
+            json.dump(users, file, indent=4)
 
     def update_user(self):
         pass
@@ -17,4 +47,5 @@ class User:
     def user_role(self):
         pass
 
-
+# usage: new_user = User('user name', 'user password', 'user role (admin)')
+# new_user.create_user()
