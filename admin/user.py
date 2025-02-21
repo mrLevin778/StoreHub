15266490aa7cfb065e1d7e5e00ec3753fd5043e2
mrLevin778 <vitaliy.respect@gmail.com>
2025-02-22 01:48:06 +0200

    Minor fixes, added logging to user.py and authsystem.py

--- admin/user.py
+++ admin/user.py
@@ -1,6 +1,9 @@
 import os
 import bcrypt
 import json
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 
 class User:
@@ -21,10 +24,12 @@ class User:
 
     @staticmethod
     def hash_password(password):
+        """Hashes the password using bcrypt"""
         return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
 
     @staticmethod
     def load_users():
+        """Loads users from the users.json file"""
         if not os.path.exists(User.FILE_PATH):
             os.makedirs(os.path.dirname(User.FILE_PATH), exist_ok=True)  # create directory
             with open(User.FILE_PATH, "w", encoding="UTF-8") as file:
@@ -33,69 +38,93 @@ class User:
             with open(User.FILE_PATH, "r", encoding="UTF-8") as file:
                 return json.load(file)
         except json.JSONDecodeError:
-            print(f'File {User.FILE_PATH} incorrect! Creating new file')
+            logging.error(f'File {User.FILE_PATH} incorrect! Creating new file')
             with open(User.FILE_PATH, "w", encoding="UTF-8") as file:
                 json.dump({}, file, indent=4)
             return {}
 
     @staticmethod
     def authenticate_user(username, password):
+        """Authenticate a user"""
         users = User.load_users()
         if username not in users:
-            return False  # user is not exist
+            logging.warning(f'User {username} does not exist.')
+            return False  # user does not exist
         hashed_password = users[username]['password']
-        return bcrypt.checkpw(password.encode(), hashed_password.encode())
+        if bcrypt.checkpw(password.encode(), hashed_password.encode()):
+            logging.info(f'User {username} authenticated successfully.')
+            return True
+        logging.warning(f'Incorrect password for user {username}.')
+        return False
 
     def create_user(self):
-        users = self.load_users()
+        """Create a new user"""
+        users = User.load_users()
         if self.username in users:
-            raise ValueError('User is existing!')
+            logging.error(f'User {self.username} already exists!')
+            raise ValueError('User already exist!')
         users[self.username] = {'password': self.password, 'role': self.role}
         self.save_users(users)
-        print(f'User {self.username} is created!')
+        logging.info(f'User {self.username} is created!')
 
+    @staticmethod
     def save_users(users):
+        """Saves the users to the JSON file"""
         os.makedirs(os.path.dirname(User.FILE_PATH), exist_ok=True)
         with open(User.FILE_PATH, "w", encoding="UTF-8") as file:
             json.dump(users, file, indent=4)
 
+    @staticmethod
     def update_password(username, old_password, new_password):
+        """Updates the password of a user"""
         users = User.load_users()
         if username not in users:
+            logging.error(f'User {username} not found.')
             raise ValueError(f'User {username} not found')
         hashed_password = users[username]['password']
         # check old password:
         if not bcrypt.checkpw(old_password.encode(), hashed_password.encode()):
+            logging.error(f'Incorrect old password for user {username}.')
             raise ValueError('Wrong old password!')
         # update password:
         users[username]['password'] = User.hash_password(new_password)
         User.save_users(users)
-        print(f'Password for {username} is updated!')
+        logging.info(f'Password for user {username} is updated!')
 
+    @staticmethod
     def delete_user(username):
+        """Deletes a user"""
         users = User.load_users()
         if username not in users:
+            logging.error(f'User {username} not found.')
             raise ValueError(f'User {username} is not found!')
         del users[username]
         User.save_users(users)
-        print(f'User {username} is deleted!')
+        logging.info(f'User {username} is deleted!')
 
+    @staticmethod
     def user_role(username):
         """Getting user role"""
         users = User.load_users()
-        return users.get(username, {}).get("role", None)
+        role = users.get(username, {}).get("role", None)
+        if role:
+            logging.info(f'User {username} has role {role}.')
+        else:
+            logging.warning(f'User {username} not found.')
+        return role
 
+    @staticmethod
     def set_user_role(username, new_role):
-        """Change user role"""
+        """Changes user role"""
+        if new_role not in User.ROLES:
+            logging.error(f'Unknown role {new_role}.')
+            return False
         users = User.load_users()
         if username not in users:
-            print('Error: user is not found')
-            return False
-        if new_role not in User.ROLES:
-            print('Error: unknown role')
+            logging.error(f'User {username} not found.')
             return False
         users[username]["role"] = new_role
         User.save_users(users)
-        print(f'User role is changed for {username}. New role - {new_role}')
+        logging.info(f'User {username} role is changed to {new_role}.')
         return True
 
--- core/authsystem.py
+++ core/authsystem.py
@@ -1,5 +1,7 @@
+import logging
 from admin.user import User
 
+logging.basicConfig(level=logging.INFO)
 
 class AuthSystem:
     """Class for login"""
@@ -8,9 +10,9 @@ class AuthSystem:
     def login(username: str, password: str):
         """login form"""
         if User.authenticate_user(username, password):
-            print('Successfully entry')
+            logging.info('Successfully logged in')
             return True
-        print('Incorrect login of password')
+        logging.warning('Incorrect login of password')
         return False
 
     @staticmethod
@@ -19,14 +21,14 @@ class AuthSystem:
         username = input('Enter login: ')
         users = User.load_users()
         if username in users:
-            print(f'User with this username is existing!')
+            logging.warning(f'User with username "{username}" already exist!')
             return None
         password = input('Enter password: ')
         hashed_password = User.hash_password(password)
-        # adding new user
+        # add new user
         users[username] = {'password': hashed_password, 'role': 'user'}
         User.save_users(users)
-        print(f'User {username} is successfully registered!')
+        logging.info(f'User {username} is successfully registered!')
 
     @staticmethod
     def start_terminal():
@@ -43,14 +45,13 @@ class AuthSystem:
             if choice == '1':
                 username = input('Enter your username: ')
                 password = input('Enter your password: ')
-                user = AuthSystem.login(username, password)
-                if user:
+                if AuthSystem.login(username, password):
                     print('Access allowed')
                     break
             elif choice == '2':
                 AuthSystem.register()
             elif choice == '3':
-                username = input('\nEnter your username, if you want check your role: ')
+                username = input('\nEnter your username to check your role: ')
                 result = User.user_role(username)
                 if result is None:
                     print(f'Error: incorrect user')
@@ -61,7 +62,7 @@ class AuthSystem:
                 role = input('\nEnter your new role: ')
                 User.set_user_role(username, role)
             elif choice == '5':
-                print('Exiting...')
+                logging.info('Exiting...')
                 break
             else:
-                print('Unknown command, please try again.')
+                logging.error('Unknown command, please try again.')
