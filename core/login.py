30d9bc0bd97b85712e9d2a657fc325d13320fd2d
mrLevin778 <vitaliy.respect@gmail.com>
2025-02-24 23:33:27 +0200

    Added method for move login window to center of screen

--- core/login.py
+++ core/login.py
@@ -12,6 +12,7 @@ class Login(ctk.CTk):
         self.auth_system = AuthSystem()
         self.title('Authorisation Form')
         self.geometry('300x250')
+        self.center_window(300, 250)
         ctk.set_appearance_mode('dark')
         ctk.set_default_color_theme('blue')
         # label
@@ -30,18 +31,32 @@ class Login(ctk.CTk):
         self.terminal_button = ctk.CTkButton(self, text='TERMINAL', command=self.open_terminal)
         self.terminal_button.pack(pady=5)
 
+    def center_window(self, width, height):
+        """Centers the window on the screen"""
+        self.update_idletasks()
+        width = self.winfo_width() or 300
+        height = self.winfo_height() or 250
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        x = (screen_width - width) // 2
+        y = (screen_height - height) // 2
+        self.geometry(f'{width}x{height}+{x}+{y}')
+
     def authenticate(self):
+        """Authenticate user and handle success or failure"""
         if self.auth_system.login(self.username_entry.get(), self.password_entry.get()):
             self.on_login_success()
         else:
             messagebox.showerror('Error', 'Incorrect login or password')
 
     def on_login_success(self):
+        """Handle successful login"""
         messagebox.showinfo('Success', 'Access granted!')
         self.destroy()
         # place for calling menu or other display
 
     def open_terminal(self):
+        """Open terminal via Core"""
         self.destroy()  # close login window
         AuthSystem.start_terminal()
             
